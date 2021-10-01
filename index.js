abe72560c51efbd1d3b86d9556151058f82e1599
Bruno Sallum <87883544+B-Sallum@users.noreply.github.com>
2021-10-01 10:50:45 -0300

    comentário

--- index.js
+++ index.js
@@ -4,11 +4,28 @@ const app = express()
 const port = 3000
 
 app.set("view engine", "ejs")
-
 app.use(express.static(path.join(__dirname, "public")))
+app.use(express.urlencoded())
 
-app.get("/", function (req, res) {
-    res.render("index")
+var pokedex = [
+    {
+        number: 0,
+        poke: "Bulbasaur"
+    }
+]
+
+app.get("/", (req, res) => {
+    res.render("index", { pokedex })
 })
+
+// app.get("/new", (req, res) => {
+//     res.render("new")
+// })
+
+// app.post("/submit", (req, res) => {
+//     const { number, poke, type, image, description, height, weight, category, skill } = req.body
+//     let pokeNew = { number, poke, type, image, description, height, weight, category, skill }
+//     pokedex.push(pokeNew)
+// })
 
 app.listen(port, () => console.log(`Servidor rodando em http://localhost:${port}`))
